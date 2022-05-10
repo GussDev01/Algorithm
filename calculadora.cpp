85d9ec754eede828d018f1a9189860d914ba42f8
unknown <acme227@gmail.com>
2022-05-10 16:10:17 -0500

    uwu

--- calculadora.cpp
+++ calculadora.cpp
@@ -4,19 +4,43 @@ using namespace std;
 
 int main() {
 
-ios_base::sync_with_stdio(0);cin.tie(0);
-int sum, resta, multiplicacion, division, num1, num2, pos;
+ios_base::sync_with_stdio(0);
+int suma, resta, multiplicacion, division, num1, num2, pos, resultado;
 
 
 cout <<"Escoja la perra operacion\n";
 cout <<"1.- Suma \n2.-Resta \n 3.-Division \n 4.-multiplicacion\n";
 
-cout<< "inserte los digitos\n";
-cin >> num1 >> num2;
- cin >> pos;
+cin>> pos;
+
     switch(pos){
-        case 1: sum = num1 + num2;
-        cout << "tu Suma es\n:" << sum;
+        case 1:
+            cout<< "suma.\n";
+            cout<<"ingrese el primer digito: ";
+            cin>> num1;
+            cout<<"ingrese el segundo digito: ";
+            cin>> num2;
+
+            resultado= num1 + num2;
+            cout<<"resultado: " <<resultado;
+
+            break;
+
+                case 2:
+                cout<< "resta. \n";
+                cout<<"ingrese el primer digito: ";
+                cin>> num1;
+                cout<<"ingrese el segundo digito: ";
+                cin>> num2;
+
+                resultado= num1 - num2;
+                cout<<"resultado: " <<resultado;
+                
+                break;
+
+                    case 3:
+                    cout<< "division. \n"
+
     }
 
 
