e873fb3d05d944d687963752a61f4a4cac1482d2
Mario Yael <marioyaelgg@gmail.com>
2022-05-10 11:21:34 -0500

    los programadores se la comen

---
+++ calculadora.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+ios_base::sync_with_stdio(0);cin.tie(0);
+
+int suma, resta, multiplicacion, división, num1, num2;
+
+cout <<"Escoja la perra operacion"
+cout <<"1.- Suma \n2.-Resta \n 3.-Divisi 
+suma = num1 + num2; 
+if resta
+
+
+
+}
