3f586cd4fe2c0cbc275d96065d1d4442b81751cc
Gusover01 <gdelangelf2100@alumno.ipn.mx>
2022-05-10 11:23:21 -0500

    asdadasd

--- code.cpp
+++ code.cpp
@@ -1,30 +1,8 @@
-<<<<<<< HEAD
-Gustavo Del Angel
-<<<<<<< HEAD
-Mario Yael Gordillo Garcia
 
-
-=======
-Mario Yael Gordillo Garcia 
-Allan Casaos Meraz
-Ivan Pablo Avelleyra Almanza
->>>>>>> 5ab220d2c8733469500872575074ddfb3a7a2345
-=======
 #include <bits/stdc++.h>
 
 using namespace std;
 int main () {
-    cout << "Hola amores\n";
-    
-    int numero;
-    cout <<"digite un numero:";
-    cin >>numero;
 
-    if(numero%2==0){
-        cout<<"El numero es par";
-    }
-    else {
-        cout<<"El numero es impar";
-    }
+   
 }
->>>>>>> 512d019320ec97db72840584f9eabc351b8d206b
