0e22662fa6c99a304bf5aeacff937990e6900f32
Gusover01 <gdelangelf2100@alumno.ipn.mx>
2022-05-10 15:35:25 -0500

    Configuracion Gustavo

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,24 +1,40 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
+int main(void) {
 
-ios_base::sync_with_stdio(0);cin.tie(0);
-int sum, resta, multiplicacion, division, num1, num2, pos;
+ios_base::sync_with_stdio(0);
+int sum, num1, num2;
+char op;
 
+    cout <<" Suma (+) \n Resta (-) \n Multiplicacion (*) \n Division (/)\n\n";
+    cout <<" Seleccione una opcion: ";
+    cin >> op;
 
-cout <<"Escoja la perra operacion\n";
-cout <<"1.- Suma \n2.-Resta \n 3.-Division \n 4.-multiplicacion\n";
+    cout<< "   Inserte los digitos: ";
+    cin >> num1 >> num2;
 
-cout<< "inserte los digitos\n";
-cin >> num1 >> num2;
- cin >> pos;
-    switch(pos){
-        case 1: sum = num1 + num2;
-        cout << "tu Suma es\n:" << sum;
+    switch (op) {
+        case '+': 
+            sum = num1 + num2;
+            cout << "\n Suma: " << num1 << " + " << num2 << " = " << sum;
+        break;
+
+        case '-': 
+            sum = num1 - num2;
+            cout << "\n Resta: " << num1 << " - " << num2 << " = " << sum;
+        break;
+
+        case '*': 
+            sum = num1 * num2;
+            cout << "\n Multiplicacion: " << num1 << " * " << num2 << " = " << sum;
+        break;
+
+        case '/': 
+            sum = num1 / num2;
+            cout << "\n Division: " << num1 << " / " << num2 << " = " << sum;
+        break;
     }
 
-
-
+return 0;
 }
