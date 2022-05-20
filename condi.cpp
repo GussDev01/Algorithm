93ebb95c4c8e1893a9cbd9d7d7c3e8805725c03c
Gusover01 <gdelangelf2100@alumno.ipn.mx>
2022-05-19 22:32:01 -0500

    Ingreso de Condicion

---
+++ condi.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int edad, ingreso,impuesto,porcentaje; 
+
+    cout<<"Ingrese su edad: \n";
+    cin>>edad;
+
+    if(edad >= 18){
+           cout<<"Eres mayor de edad, Ingreso mensual: \n";
+            cin>>ingreso;
+
+        if(ingreso >= 4000){
+            
+            impuesto = (ingreso *.10); 
+            porcentaje = ingreso - impuesto ;
+            cout<<"impuesto a declarar: "<<impuesto;
+            cout<<"\nSueldo restante: "<<porcentaje;
+        }
+        else{
+               cout<<"Ganas muy poco.";
+        }
+    
+    }
+    else
+        cout<<"Eres menor de edad, saquese alv.";        
+}
--- vectores.cpp
+++ vectores.cpp
@@ -2,14 +2,18 @@
 
 using namespace std; 
 
+
+
 int main(){
-
-
-
-    vector<string> names {"Juan","Pablo", "Allan"};
+                              //   [0,1,2]
+    vector<string> names {"nom1","nom2", "nom3"};
     vector<float> decimales {2.1, 3.3, 5.0, 7.0, 11.0, 13.0, 17.4, 19.0};
 
+
+   
     
+//vector "arreglo indefinido"
+
     auto Calif_Mario = vector<int>{}; /* [0,1,2,3,4,5,6, infinitos] */
 
         Calif_Mario.push_back(35);  // Ok: [3]
@@ -23,7 +27,6 @@ int main(){
         Calif_Mario.push_back(31);  // Ok: [3, 1, 2]
         Calif_Mario.push_back(92);  // Ok: [3, 1, 2]
         Calif_Mario.push_back(54);  // Ok: [3, 1, 2]
-
 
         for(int i = 0; i < 11; i++)
         cout <<"\n"<< Calif_Mario[i];
